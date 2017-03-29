82a109c659966290849b2d728f8d29d5e7beb0e2
Ferlargut <ferlargut@gmail.com>
2017-03-29 11:07:08 -0600

    dandovidaAlBoton

--- js/js.js
+++ js/js.js
@@ -1,10 +1,10 @@
-var listas={};
+
 function Lista(titulo) {
     this.titulo=titulo; 
-    this.lista="";   
+    this.pendiente="";   
    
 }
-
+ var listas=[];
 function crearLista() {
     var lugar=document.getElementById("listas");
     var titulo=document.createElement("h2");
@@ -12,15 +12,34 @@ function crearLista() {
     var inputLista=document.createElement("input");
     var lista=document.createElement("ul")
     var elemento=new Lista(document.getElementById("listaNew").value);
-
+    listas.push(elemento);
     lista.id="ponerListas";
-    inputLista.id="obtenerComentarios"
-   
+    inputLista.id="obtenerComentarios";      
     lugar.appendChild(titulo);
     lugar.appendChild(lista);
     lugar.appendChild(inputLista);
     lugar.appendChild(boton);
+    boton.id="boton";
     titulo.innerHTML=elemento.titulo;
+    boton.innerText=boton.name="Pendientes";
+    boton=document.getElementById("boton");
+    boton.setAttribute("onClick", "crearPendientes();");
+
+
+
+    
+}
+
+function crearPendientes() {
+    var lugar = document.getElementById("ponerListas");
+    var pendiente=document.getElementById("obtenerComentarios").value;
+    var crearPend=document.createElement("li");
+    var posicion=document.getElementById("espacio");
+    crearPend.id="espacio";   
+    
+    crearPend.id="pendienteDinamico"
+    lugar.appendChild(crearPend);
+    
     
 
 
