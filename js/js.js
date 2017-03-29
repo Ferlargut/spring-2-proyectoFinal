35e55cfc23395cf519a152fc7553c085206c468b
Ferlargut <ferlargut@gmail.com>
2017-03-29 10:06:49 -0600

    Creaandopendientes

--- js/js.js
+++ js/js.js
@@ -1,1 +1,28 @@
-alert("hola");
+var listas={};
+function Lista(titulo) {
+    this.titulo=titulo; 
+    this.lista="";   
+   
+}
+
+function crearLista() {
+    var lugar=document.getElementById("listas");
+    var titulo=document.createElement("h2");
+    var boton=document.createElement("button");
+    var inputLista=document.createElement("input");
+    var lista=document.createElement("ul")
+    var elemento=new Lista(document.getElementById("listaNew").value);
+
+    lista.id="ponerListas";
+    inputLista.id="obtenerComentarios"
+   
+    lugar.appendChild(titulo);
+    lugar.appendChild(lista);
+    lugar.appendChild(inputLista);
+    lugar.appendChild(boton);
+    titulo.innerHTML=elemento.titulo;
+    
+
+
+    
+}
